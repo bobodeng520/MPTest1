678efc34fd3e11a7895b6130e229ed482e5e4661
MilesDeng <MilesDeng@viatech.com.cn>
2016-07-05 15:04:27 +0800

    Add source code to this project with MPChartLib module

---
+++ app/src/main/java/com/example/root/mptest1/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.root.mptest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LineChart lineChart = (LineChart) findViewById(R.id.chart);
+        // creating list of entry
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 4f));
+        entries.add(new Entry(1, 8f));
+        entries.add(new Entry(2, 6f));
+        entries.add(new Entry(3, 2f));
+        entries.add(new Entry(4, 18f));
+        entries.add(new Entry(5, 9f));
+
+        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+        // creating labels
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(dataset); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(dataSets);
+        lineChart.setData(data); // set the data and list of lables into chart
+
+        lineChart.setDescription("Description");  // set the description
+    }
+}
